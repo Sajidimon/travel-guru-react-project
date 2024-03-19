0616ada8c1baa65fa4b80a0e4614ed450de8ae15
sajidimon <imonchowdhury6@gmail.com>
2024-03-19 19:44:51 +0600

    hero section completed

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,20 +1,39 @@
 import Navbar from "../Navbar/Navbar";
 import './Header.css';
 import { FaLongArrowAltRight } from "react-icons/fa";
+import Marquee from "react-fast-marquee";
+import slide1 from '../../assets/Sajek.png';
+import slide2 from '../../assets/Sreemongol.png';
+import slide3 from '../../assets/sundorbon.png';
 
 const Header = () => {
     return (
         <>
             <div className="header">
                 <Navbar></Navbar>
-                <div>
-                    <div>
-                        <h2>Coxs bazar</h2>
-                        <p>Coxs Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                <div className="flex">
+                    <div className="hero-left">
+                        <h2 className="text-4xl uppercase">Coxs bazar</h2>
+                        <p className="m-4">Coxs Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
                         <button className="btn btn-warning">Booking <FaLongArrowAltRight></FaLongArrowAltRight></button>
                     </div>
                     <div>
-
+                        <div className="carousel justify-center items-center w-full">
+                            <div id="item1" className="carousel-item w-full">
+                                <img src={slide1} className="w-full" />
+                            </div>
+                            <div id="item2" className="carousel-item w-full">
+                                <img src={slide2} className="w-full" />
+                            </div>
+                            <div id="item3" className="carousel-item w-full">
+                                <img src={slide3} className="w-full" />
+                            </div>
+                        </div>
+                        <div className="flex justify-center w-full py-2 gap-2">
+                            <a href="#item1" className="btn btn-xs">1</a>
+                            <a href="#item2" className="btn btn-xs">2</a>
+                            <a href="#item3" className="btn btn-xs">3</a>
+                        </div>
                     </div>
                 </div>
             </div>
