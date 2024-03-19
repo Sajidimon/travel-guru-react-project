891a75cc5fc48ab7ceff711a386638c9c7d65e2f
sajidimon <imonchowdhury6@gmail.com>
2024-03-19 17:45:19 +0600

    installed react marquee

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -9,12 +9,12 @@ const Header = () => {
                 <Navbar></Navbar>
                 <div>
                     <div>
-                        <h2>Cox's bazar</h2>
-                        <p>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                        <h2>Coxs bazar</h2>
+                        <p>Coxs Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
                         <button className="btn btn-warning">Booking <FaLongArrowAltRight></FaLongArrowAltRight></button>
                     </div>
                     <div>
-                        
+
                     </div>
                 </div>
             </div>
