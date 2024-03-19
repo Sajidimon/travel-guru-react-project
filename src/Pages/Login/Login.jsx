04a875696db1f6a4f9e23c280025b79109630a6f
sajidimon <imonchowdhury6@gmail.com>
2024-03-19 21:07:41 +0600

    login page design

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,13 +6,9 @@ const Login = () => {
         <>
             <Navbar></Navbar>
             <div>
-                <div className="hero min-h-screen bg-base-200">
-                    <div className="hero-content flex-col lg:flex-row-reverse">
-                        <div className="text-center lg:text-left">
-                            <h1 className="text-5xl font-bold">Login now!</h1>
-                            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        </div>
-                        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="hero min-h-screen">
+                    <div className="">
+                        <div className="card shadow-2xl bg-base-100">
                             <form className="card-body">
                                 <div className="form-control">
                                     <label className="label">
@@ -33,6 +29,13 @@ const Login = () => {
                                     <button className="btn btn-primary">Login</button>
                                 </div>
                             </form>
+                        </div>
+                        <div>
+                            <div className="divider">OR</div>
+                            <button className="btn btn-outline btn-primary btn-wide">Continue with Facebook</button>
+                            <br />
+                            <br />
+                            <button className="btn btn-outline btn-warning btn-wide">Continue with Google</button>
                         </div>
                     </div>
                 </div>
