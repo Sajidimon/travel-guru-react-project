214186111b1ceafe43f9d64069b23bf1a8e87ee0
sajidimon <imonchowdhury6@gmail.com>
2024-03-20 11:29:24 +0600

    signin system completed

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,27 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../Layouts/Navbar/Navbar";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../Firebase/Firebase.init";
 
 
 const Login = () => {
+
+
+    const handlelogin = e => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.error(error)
+        })
+    }
+
     return (
         <>
             <Navbar></Navbar>
@@ -10,18 +29,18 @@ const Login = () => {
                 <div className="hero min-h-screen">
                     <div className="">
                         <div className="card shadow-2xl bg-base-100">
-                            <form className="card-body">
+                            <form onSubmit={handlelogin} className="card-body">
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="email" placeholder="email" className="input input-bordered" required />
+                                    <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" placeholder="password" className="input input-bordered" required />
+                                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                                     <label className="label">
                                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>
@@ -31,13 +50,6 @@ const Login = () => {
                                 </div>
                                 <p className="text-white">Don't Have an account ? <Link to="/registration"><span className="text-yellow-500 font-bold">Create new account</span></Link> </p>
                             </form>
-                        </div>
-                        <div>
-                            <div className="divider">OR</div>
-                            <button className="btn btn-outline btn-primary btn-wide">Continue with Facebook</button>
-                            <br />
-                            <br />
-                            <button className="btn btn-outline btn-warning btn-wide">Continue with Google</button>
                         </div>
                     </div>
                 </div>
