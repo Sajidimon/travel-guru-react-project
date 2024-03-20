27058f8ff2ba288f6d8aa986e4f0cf514a20f2a8
sajidimon <imonchowdhury6@gmail.com>
2024-03-20 11:11:41 +0600

    create user with email & password

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,8 +1,26 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../Layouts/Navbar/Navbar";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../Firebase/Firebase.init";
 
 
 const Registration = () => {
+
+
+    const handleregistration = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+            console.log(result.user)
+            })
+            .catch(error => {
+            console.error(error)
+        })
+    }
+
     return (
         <>
             <Navbar></Navbar>
@@ -17,7 +35,7 @@ const Registration = () => {
                             <button className="btn btn-outline btn-warning btn-wide">Continue with Google</button>
                         </div>
                         <div className="card shrink-0 w-full max-w-sm shadow-2xl">
-                            <form className="card-body">
+                            <form onSubmit={handleregistration} className="card-body">
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Full Name</span>
@@ -28,13 +46,13 @@ const Registration = () => {
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="email" placeholder="email" className="input bg-white input-bordered" required />
+                                    <input type="email" name="email" placeholder="email" className="input bg-white input-bordered" required />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" placeholder="password" className="input bg-white input-bordered" required />
+                                    <input type="password" name="password" placeholder="password" className="input bg-white input-bordered" required />
                                     <label className="label">
                                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>
