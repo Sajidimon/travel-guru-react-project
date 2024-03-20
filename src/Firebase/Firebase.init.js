c3dc41ae76bf717463b2717eec47596b391f4237
sajidimon <imonchowdhury6@gmail.com>
2024-03-20 11:13:04 +0600

    code modified

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -1,10 +1,13 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
     apiKey: "AIzaSyC1PkP7y9ntfAtbnGb3ioUh_UOX8ZqkQsc",
     authDomain: "travel-guru-react-project.firebaseapp.com",
@@ -15,6 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
