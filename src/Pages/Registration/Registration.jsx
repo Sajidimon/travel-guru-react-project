e962537e2462019ef549849658daee6bdd87fda3
sajidimon <imonchowdhury6@gmail.com>
2024-03-20 16:00:36 +0600

    email & password validation

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -2,9 +2,17 @@ import { Link } from "react-router-dom";
 import Navbar from "../../Layouts/Navbar/Navbar";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import auth from "../../Firebase/Firebase.init";
+import { useState } from "react";
+import { IoIosEyeOff, IoMdEye } from "react-icons/io";
 
 
 const Registration = () => {
+
+    const [registerError, setRegisterError] = useState('');
+    const [successMessage, setSuccessMessage] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+
+    //create google login system
 
     const googleLogin = () => {
 
@@ -24,12 +32,29 @@ const Registration = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
 
+
+        if (password.length < 6) {
+            setRegisterError('password should be at least 6 character or more')
+            return;
+        } else if (!/[A-Z]/.test(password)) {
+            setRegisterError('Password must have one Uppercase');
+            return;
+        }
+
+        //reset error
+        setRegisterError('');
+        setSuccessMessage('');
+
+        //create user
+
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 console.log(result.user)
+                setSuccessMessage('Account created successfully')
             })
             .catch(error => {
                 console.error(error)
+                setRegisterError('Email already in use');
             })
 
     }
@@ -65,7 +90,16 @@ const Registration = () => {
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" name="password" placeholder="password" className="input bg-white input-bordered" required />
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        name="password"
+                                        placeholder="password"
+                                        className="input bg-white input-bordered" required />
+                                    <span onClick={() => setShowPassword(!showPassword)}>
+                                        {
+                                            showPassword ? <IoIosEyeOff></IoIosEyeOff> : <IoMdEye></IoMdEye>
+                                        }
+                                    </span>
                                     <label className="label">
                                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>
@@ -75,6 +109,12 @@ const Registration = () => {
                                 </div>
                                 <p className="text-black">Already Have an account ? <Link to="/login"><span className="text-yellow-500 font-bold">Login</span></Link> </p>
                             </form>
+                            {
+                                registerError && <p className="text-red-600">{registerError}</p>
+                            }
+                            {
+                                successMessage && <p className="text-green-600">{successMessage}</p>
+                            }
                         </div>
                     </div>
                 </div>
